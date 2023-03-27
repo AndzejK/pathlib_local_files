c34919b0d7ddb04e9f1ea786ff69ce7f07b442c9
Andzej Krupoves <andrejkrupovec@gmail.com>
2023-03-27 09:34:47 +0300

    change just prefix with with_suffix

--- change_extention_of_a_file.py
+++ change_extention_of_a_file.py
@@ -11,8 +11,13 @@ path_to_the_files=root/dir/subdir
 # Now I'm going loop through dir to see what I have in that dir
 files=path_to_the_files.iterdir()
 extentions=['.csv',['mp3']]
-for file in files: 
-    get_stem=file.stem
-    new_extention=get_stem+extentions[0] # the original name and changing extention to .cvs
-    new_file_name=file.with_name(new_extention) # here I construct a name/ext of a file
-    file.rename(new_file_name)
+
+#for file in files: 
+#    get_stem=file.stem
+#    new_extention=get_stem+extentions[0] # the original name and changing extention to .cvs
+#    new_file_name=file.with_name(new_extention) # here I construct a name/ext of a file
+#    file.rename(new_file_name)
+
+for file in files:
+    new_filepath=file.with_suffix(".csv") # with_suffix changes just extention 
+    file.rename(new_filepath)
