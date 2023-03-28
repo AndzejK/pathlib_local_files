b297c7c3b68ba4523947b060342f6e80a3e94f62
Andzej Krupoves <andrejkrupovec@gmail.com>
2023-03-28 09:30:46 +0300

    Search files in Downloads dir

---
+++ search.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import re
+downloads=Path("/Users/rock/Downloads/")
+print(downloads.absolute())
+file_count=0
+dir_count=0
+
+for file in downloads.glob("*"):
+    
+    if file.is_file():
+        #file_count+=1
+        match_jpg=re.search(r".*\.jpg",file.name) #patter for each line in file variable to search for
+        if match_jpg:
+            file_count+=1
+            print(match_jpg.group())
+            print(f"File: {file.name}")
+    else:
+        dir_count+=1
+        print(f"Directory: {file.name}")
+print(f"\nTotal Files: {file_count}\nTotal dirs: {dir_count}")
